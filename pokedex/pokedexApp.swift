1a3a97845733ebbf0cb88d3bec5e53ec80614e2a
gemanepa <contact@gemanepa.com>
2024-01-07 22:23:04 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Gabriel Ernesto Martinez Canepa on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
